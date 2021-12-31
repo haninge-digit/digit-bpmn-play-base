1de5e0bb6e47b3f3f8d25e2ec47d08d94085ae24
hakan-persson <hakan.persson@haninge.se>
2021-12-31 18:43:34 +0100

    Added some output

--- deployToCamunda.py
+++ deployToCamunda.py
@@ -13,6 +13,9 @@ def deployToCamunda():
                 # print(f)
                 m = MultipartEncoder(fields={'deployment-name': f.split(".")[0], 'deployment-source': 'Digi:T Deployer', 'deploy-changed-only': 'true', f: (f, open(f, 'rb'), 'text/xml')})
                 r = requests.post(f"{CAMUNDA}/deployment/create", data=m, headers={'Content-Type': m.content_type})
+                pres = r.json()
+                depstr = "Already " if pres['deployedProcessDefinitions'] == None else ""
+                print(f"{f}: {depstr}deployed")
                 # print(json.dumps(r.json(), indent=4, sort_keys=True))
 
 if __name__ == '__main__':
