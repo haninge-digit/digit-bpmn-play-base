953d95f6a12fe81f0eee2996cce6e10d9eb5e4b9
hakan-persson <hakan.persson@haninge.se>
2022-06-29 11:24:49 +0200

    Cleaned up version without a working action

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Playing with same base BPMN
+Contains some "playable" BPMN-processes
+
+Repo contains a set of BPMN-files as well as a "Camunda deployer", that deplays all the processes in the directory to Camunda. Can be run locally after cocceting to Camunda with 
+```bash
+kubectl port-forward svc/camunda8-zeebe-gateway 26500:26500 -n camunda8
+```
+
+Deployment through a Github action is on it's way...
 
 ## echo.bpmn
 A process with two actions. One is an external worker that does nothing 
--- deployToCamunda.py
+++ deployToCamunda.py
@@ -1,42 +1,26 @@
 import logging
 import os
-import requests
-from requests_toolbelt import MultipartEncoder
-import json
 import grpc
 from zeebe_grpc import gateway_pb2, gateway_pb2_grpc
 
-CAMUNDA = os.getenv('CAMUNDA','http://camunda-api-server:8080/engine-rest')   # This is the default
+ZEEBE_ADDRESS = os.getenv('ZEEBE_ADDRESS',"localhost:26500")
 
 def deployToCamunda():
-    with grpc.insecure_channel("localhost:26500") as channel:
+    with grpc.insecure_channel(ZEEBE_ADDRESS) as channel:
         stub = gateway_pb2_grpc.GatewayStub(channel)
-        for (_, _, files) in os.walk('.', topdown=True):
-            for f in files:
-                if ".bpmn" in f:
-                    # print(f)
-                    with open(f, "rb") as process_definition_file:
-                        process_definition = process_definition_file.read()
-                        process = gateway_pb2.ProcessRequestObject(name=f,definition=process_definition)
-                        stub.DeployProcess(gateway_pb2.DeployProcessRequest(processes=[process]))
-
-
-                    # m = MultipartEncoder(fields={'deployment-name': f.split(".")[0], 'deployment-source': 'Digi:T Deployer', 'deploy-changed-only': 'true', f: (f, open(f, 'rb'), 'text/xml')})
-                    # r = requests.post(f"{CAMUNDA}/deployment/create", data=m, headers={'Content-Type': m.content_type})
-                    # pres = r.json()
-                    depstr = "Already " if pres['deployedProcessDefinitions'] == None else ""
-                    print(f"{f}: {depstr}deployed")
-                    # print(json.dumps(r.json(), indent=4, sort_keys=True))
+        for f in os.listdir():      # Don't traverse the directory for the moment
+            if ".bpmn" in f:
+                logging.info(f"Trying to deploy {f} to Camunda")
+                with open(f, "rb") as process_definition_file:
+                    process_definition = process_definition_file.read()
+                    process = gateway_pb2.Resource(name=f,content=process_definition)
+                    response = stub.DeployResource(gateway_pb2.DeployResourceRequest(resources=[process]))
+                    logging.info(f"Deployed BPMN process {response.deployments[0].process.bpmnProcessId} as version {response.deployments[0].process.version}")
 
 if __name__ == '__main__':
-    logging.info("Starting "+__name__)
+    if os.getenv('DEBUG','false') == "true": # Debug requested
+        logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")     # Default logging level
+
+    logging.info(f"Starting deployToCamunda")
     deployToCamunda()
-
-
-
-
-    # print the topology of the zeebe cluster
-    topology = stub.Topology(gateway_pb2.TopologyRequest())
-    print(topology)
-
-    # deploy a process definition
